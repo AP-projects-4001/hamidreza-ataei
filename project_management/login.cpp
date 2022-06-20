25f9afe7954435ccf7fd4703ee858095daa65969
Hamidreza-Ataei <95142224+Hamidreza-Ataei@users.noreply.github.com>
2022-06-21 01:06:03 +0430

    create login page

--- project_management/login.cpp
+++ project_management/login.cpp
@@ -1,9 +1,9 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QMessageBox>
+#include <QFile>
 
-login::login(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::login)
+login::login(QWidget *parent) : QMainWindow(parent) , ui(new Ui::login)
 {
     ui->setupUi(this);
 }
@@ -13,3 +13,57 @@ login::~login()
     delete ui;
 }
 
+void login::on_pushButton_2_clicked()
+{
+
+}
+
+void login::on_pushButton_3_clicked()
+{
+    if (ui->lineEdit->text() == "" || ui->lineEdit_2->text() == "")
+    {
+        QMessageBox *msgBox = new QMessageBox(this);
+        msgBox->setWindowTitle("Error");
+        msgBox->setStyleSheet("QLabel{min-width: 200px; color: rgb(171, 171, 171); font: 75 12pt Georgia;}");
+        msgBox->setInformativeText("fields are required ✘");
+        msgBox->exec();
+        return;
+    }
+    QFile file("C:/Users/3atae/Desktop/project/accounts_data.txt");
+    file.open(QIODevice::ReadOnly);
+    bool chech = 0;
+    while (!file.atEnd())
+    {
+        QString temp = file.readLine();
+        QString username = file.readLine();
+        temp = file.readLine();
+        temp = file.readLine();
+        QString password = file.readLine();
+        if (ui->lineEdit->text() + '\n' == username && ui->lineEdit_2->text() + '\n' == password)
+        {
+            chech = 1;
+            break;
+        }
+    }
+    file.close();
+    if (chech == 0)
+    {
+        QMessageBox *msgBox = new QMessageBox(this);
+        msgBox->setWindowTitle("Error");
+        msgBox->setStyleSheet("QLabel{min-width: 200px; color: rgb(171, 171, 171); font: 75 12pt Georgia;}");
+        msgBox->setInformativeText("username is wrong ✘");
+        msgBox->exec();
+        return;
+    }
+    QMessageBox *msgBox = new QMessageBox(this);
+    msgBox->setWindowTitle("Successful Log In");
+    msgBox->setStyleSheet("QLabel{min-width: 200px; color: rgb(171, 171, 171); font: 75 12pt Georgia;}");
+    msgBox->setInformativeText("successfully log in ✔");
+    this->close();
+    msgBox->exec();
+}
+
+void login::on_pushButton_clicked()
+{
+
+}
--- project_management/login.h
+++ project_management/login.h
@@ -15,6 +15,13 @@ public:
     login(QWidget *parent = nullptr);
     ~login();
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::login *ui;
 };
